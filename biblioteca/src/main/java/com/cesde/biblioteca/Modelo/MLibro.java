b7efb5495d28cc641a305f2604bdd6df93a325b7
JosieEsteban <lkuro123@gmail.com>
2026-02-06 13:46:25 -0500

    Se comienza el desarrollo con de la capa Controlador Libro

--- biblioteca/src/main/java/com/cesde/biblioteca/Modelo/MLibro.java
+++ biblioteca/src/main/java/com/cesde/biblioteca/Modelo/MLibro.java
@@ -1,5 +1,15 @@
 package com.cesde.biblioteca.Modelo;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "libro")
+
 public class MLibro
 {
+
+
+
 }
